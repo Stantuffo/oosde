692520583e801214f163d014383ef71bffcca6cc
stantuffo <simon.bucciarelli@gmail.com>
2018-09-11 14:05:37 +0200

    added javadoc to ArtworkController

--- src/main/java/com/univaq/oosde/controller/ArtworkController.java
+++ src/main/java/com/univaq/oosde/controller/ArtworkController.java
@@ -4,6 +4,7 @@ import com.univaq.oosde.model.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.io.File;
@@ -13,11 +14,23 @@ import java.text.ParseException;
 import java.util.List;
 import java.util.TreeMap;
 
+/**
+ * @author  Simone;
+ * @version 1
+ **/
 @Controller
 public class ArtworkController {
 
+
+    /**
+     * @param artId         The id of the Artwork to be shown.
+     * @param request       HttpServletRequest.
+     * @return              If logged, the ModelAndView containing all the Artwork data and the template page to be rendered; if not logged, a redirect to the Login Page.
+     * @throws SQLException Thrown if data manipulation error arises.
+     */
     @GetMapping("DigitalLibrary/Artwork")
     public ModelAndView viewArtworkPage(@RequestParam int artId, HttpServletRequest request) throws SQLException {
+
         HttpSession session = request.getSession(false);
         if (session == null) {
             return new ModelAndView("redirect:/DigitalLibrary");
@@ -35,6 +48,11 @@ public class ArtworkController {
         return mav;
     }
 
+    /**
+     * @param request       HttpServletRequest.
+     * @return              If logged, the ModelAndView containing all the Category List data,the Author and the template page to be rendered; if not logged, a redirect to the Login Page.
+     * @throws SQLException Thrown if data manipulation error arises.
+     */
     @RequestMapping("DigitalLibrary/NewArtwork")
     public ModelAndView newArtworkPage(HttpServletRequest request) throws SQLException {
         HttpSession session = request.getSession(false);
@@ -57,6 +75,11 @@ public class ArtworkController {
         } else return new ModelAndView("redirect:/DigitalLibrary");
     }
 
+    /**
+     * @param request       HttpServletRequest.
+     * @return              If logged, the ModelAndView containing all the Assigned Transcription data and the template page to be rendered; if not logged, a redirect to the Login Page.
+     * @throws SQLException Thrown if data manipulation error arises.
+     */
     @RequestMapping("DigitalLibrary/MyTranscriptions")
     public ModelAndView myTranscriptions(HttpServletRequest request) throws SQLException {
         HttpSession session = request.getSession(false);
@@ -79,6 +102,12 @@ public class ArtworkController {
         } else return new ModelAndView("redirect:/DigitalLibrary");
     }
 
+    /**
+     * @param artId             The id of the Artwork to be shown.
+     * @param request           HttpServletRequest.
+     * @return                  If logged, the ModelAndView containing all the Artwork data and the template page to be rendered; if not logged, a redirect to the Login Page.
+     * @throws SQLException     thrown if data manipulation error arises.
+     */
     @GetMapping("DigitalLibrary/MyTranscriptions/Artwork")
     public ModelAndView myArtworkTranscriptions(@RequestParam int artId, HttpServletRequest request) throws SQLException {
         HttpSession session = request.getSession(false);
@@ -100,6 +129,18 @@ public class ArtworkController {
         }
     }
 
+    /**
+     * @param title         The title of the Artwork to be added.
+     * @param description   The description of the Artwork to be added.
+     * @param isbn          The title of the Artwork to be added.
+     * @param year          The title of the Artwork to be added.
+     * @param language      The title of the Artwork to be added.
+     * @param category      The title of the Artwork to be added.
+     * @param author        The title of the Artwork to be added.
+     * @param request       HttpServletRequest.
+     * @return              If logged and if the insertion is successful, a redirect to a success page, if the insertion is not successful a redirect to an error page; if not logged, a redirect to the Login Page.
+     * @throws SQLException Thrown if data manipulation error arises.
+     */
     @RequestMapping(value = "/DigitalLibrary/AddArtwork", method = RequestMethod.POST)
     public ModelAndView insertNewArtwork(@RequestParam("inputTitle") String title, @RequestParam("inputDescription") String description, @RequestParam("inputIsbn") String isbn, @RequestParam("inputYear") int year, @RequestParam("inputLanguage") String language, @RequestParam("inputCategory") int category, @RequestParam("inputAuthors") String[] author, HttpServletRequest request) throws SQLException {
         HttpSession session = request.getSession(false);
@@ -129,6 +170,11 @@ public class ArtworkController {
         }
     }
 
+    /**
+     * @param request           HttpServletRequest.
+     * @return                  If logged, the ModelAndView containing all the data and the page allowing a trascriber to choose the Artwork to transcribe; if not logged, a redirect to the Login Page.
+     * @throws SQLException     Thrown if data manipulation error arises.
+     */
     @RequestMapping(value = "/DigitalLibrary/AssignTranscription")
     public ModelAndView assignTranscription(HttpServletRequest request) throws SQLException {
         HttpSession session = request.getSession(false);
@@ -149,6 +195,12 @@ public class ArtworkController {
         }
     }
 
+    /**
+     * @param artId             The id of the Artwork to be shown.
+     * @param request           HttpServletRequest.
+     * @return                  If logged, the ModelAndView containing all the Images of a specific Artwork, a list of all the transcribers and the template page to be rendered; if not logged, a redirect to the Login Page.
+     * @throws SQLException     Thrown if data manipulation error arises.
+     */
     @GetMapping(value = "/DigitalLibrary/AssignTranscription/ChooseTranscriber")
     public ModelAndView chooseTranscriber(@RequestParam int artId, HttpServletRequest request) throws SQLException {
         HttpSession session = request.getSession(false);
@@ -171,7 +223,13 @@ public class ArtworkController {
         }
     }
 
-
+    /**
+     * @param inputUser         The id of the Transcriber.
+     * @param inputUser         The id list of all the images assigne to the Transcriber.
+     * @param request           HttpServletRequest.
+     * @return                  If logged, a success page after the assignment of all the Images to the selected Transcriber and the template page itself; if not logged, a redirect to the Login Page.
+     * @throws SQLException     Thrown if data manipulation error arises.
+     */
     @PostMapping(value = "/DigitalLibrary/AssignTranscription/AssignResult")
     public ModelAndView insertAssignment(@RequestParam("inputUser") int inputUser,
                                          @RequestParam("inputImageId") int[] inputImageId, HttpServletRequest request) throws SQLException {
@@ -202,6 +260,11 @@ public class ArtworkController {
         }
     }
 
+    /**
+     * @param request       HttpServletRequest.
+     * @return              If logged, the ModelAndView containing a list of unvalidated transcriptions willing to be validated and the template page to be rendered; if not logged, a redirect to the Login Page.
+     * @throws SQLException Thrown if data manipulation error arises.
+     */
     @RequestMapping(value = "/DigitalLibrary/Artwork/ValidateTranscription")
     public ModelAndView getTranscriptionsToBeValidated(HttpServletRequest request) throws SQLException {
         HttpSession session = request.getSession(false);
@@ -222,6 +285,12 @@ public class ArtworkController {
         }
     }
 
+    /**
+     * @param pageId        The id of the page whose transcription is being validating.
+     * @param request       HttpServletRequest.
+     * @return              If logged, a page where Administrators and Manager can review and validate the transcription; if not logged, a redirect to the Login Page.
+     * @throws SQLException Thrown if data manipulation error arises.
+     */
     @GetMapping(value = "/DigitalLibrary/Artwork/ValidateTranscription/Validate")
     public ModelAndView validateTr(@RequestParam("pageId") int pageId, HttpServletRequest request) throws SQLException {
         HttpSession session = request.getSession(false);
@@ -235,6 +304,13 @@ public class ArtworkController {
         return mav;
     }
 
+    /**
+     * @param imgId         The id of the Image associated to the transcriptioin to validate.
+     * @param transcription The text of the transcription itself.
+     * @param request       HttpServletRequest.
+     * @return              A redirect to the main page
+     * @throws SQLException Thrown if data manipulation error arises.
+     */
     @PostMapping("/DigitalLibrary/ValidateTranscription")
     public String validateTranscription(@RequestParam("imgId") int imgId, @RequestParam("transcription") String transcription, HttpServletRequest request) throws SQLException {
         HttpSession session = request.getSession(false);
@@ -244,6 +320,11 @@ public class ArtworkController {
         return "redirect:/DigitalLibrary";
     }
 
+    /**
+     * @param request       HttpServletRequest.
+     * @return              If logged, the ModelAndView containing a list of unvalidated Images willing to be validated and the template page to be rendered; if not logged, a redirect to the Login Page.
+     * @throws SQLException Thrown if data manipulation error arises.
+     */
     @RequestMapping(value = "/DigitalLibrary/Artwork/ValidateImage")
     public ModelAndView getImagesToBeValidated(HttpServletRequest request) throws SQLException {
         HttpSession session = request.getSession(false);
@@ -264,6 +345,14 @@ public class ArtworkController {
         }
     }
 
+    /**
+     * @param imgId             The id of the Image to be validated.
+     * @param request           HttpServletRequest.
+     * @return                  If logged, the ModelAndView containing the image to be validated and all its attributes and the template page to be rendered; if not logged, a redirect to the Login Page.
+     * @throws SQLException     Thrown if data manipulation error arises.
+     * @throws IOException      Thrown if IO error arises.
+     * @throws ParseException   Thrown if parsing error arises.
+     */
     @GetMapping(value = "/DigitalLibrary/Artwork/ValidateImage/Validate")
     public ModelAndView getImagesToBeValidated(@RequestParam("imgId") int imgId, HttpServletRequest request) throws SQLException, IOException, ParseException {
         HttpSession session = request.getSession(false);
@@ -282,6 +371,12 @@ public class ArtworkController {
         return mav;
     }
 
+    /**
+     * @param imgId         The id of the image to be finally validated.
+     * @param request       HttpServletRequest.
+     * @return              A redirect to the main page.
+     * @throws SQLException Thrown if parsing error arises.
+     */
     @PostMapping("/DigitalLibrary/ValidateImage")
     public String validateImage(@RequestParam("imgId") int imgId, HttpServletRequest request) throws SQLException {
         HttpSession session = request.getSession(false);
